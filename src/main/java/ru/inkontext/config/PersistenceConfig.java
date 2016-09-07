c3c013048f9d532344c865751bf483432019b930
stolbovd <stolbovd@gmail.com>
2016-09-07 20:28:53 +0500

    create project

---
+++ src/main/java/ru/inkontext/config/PersistenceConfig.java
@@ -0,0 +1,17 @@
+package ru.inkontext.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.projection.ProjectionFactory;
+import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
+
+@Configuration
+@EnableJpaRepositories(basePackages="ru.inkontext.persistence")
+public class PersistenceConfig {
+
+	@Bean
+	public ProjectionFactory projectionFactory() {
+		return new SpelAwareProxyProjectionFactory();
+	}
+}
---
+++ src/main/java/ru/inkontext/core/domain/Person.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ru.inkontext.core.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Getter @Setter
+public class Person implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column
+	private String name;
+
+	@Column
+	private String birthday;
+
+	@OneToOne
+//	@NaturalId
+	private Adress adress;
+
+	protected Person() {
+	}
+}
